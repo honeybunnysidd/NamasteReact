7372268472a74c1587ed41137e695e856843b098
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-07-25 00:46:31 +0530

    Functional and Class Components

--- FoodPlaza/src/components/UserCard.jsx
+++ FoodPlaza/src/components/UserCard.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 const UserCard = ({ name, location }) => {
+  const [count, setCount] = useState(0);
   return (
     <>
       <div className="userCard">
+        <h2>Count = {count}</h2>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          Count++
+        </button>
         <h3>Name: {name}</h3>
         <p>Location: {location} </p>
         <p>Contact: sidd@gmail.com</p>
--- FoodPlaza/src/components/UserClass.jsx
+++ FoodPlaza/src/components/UserClass.jsx
@@ -2,12 +2,23 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      count: 0,
+    };
   }
+
   render() {
     const { name, location } = this.props;
+    const { count } = this.state;
     return (
       <>
         <div className="userCard">
+          <h2>Count = {count}</h2>
+          <button onClick={() => this.setState({ count: count + 1 })}>
+            Count++
+          </button>
+
           <h3>Name: {name}</h3>
           <h4>Location:{location} </h4>
           <h4>Contact: </h4>
