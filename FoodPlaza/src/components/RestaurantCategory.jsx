15a18593cb6629bc2f737f81c08454fc1bcdc9ca
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-07-31 07:29:13 +0530

    fixing bugs

--- FoodPlaza/src/components/RestaurantCategory.jsx
+++ FoodPlaza/src/components/RestaurantCategory.jsx
@@ -1,18 +1,26 @@
+import { useState } from "react";
 import ItemList from "./ItemListAccordian";
 
 const RestaurantCategory = ({ data }) => {
+  const [showItem, setShowItem] = useState(false);
+  const handleClick = () => {
+    setShowItem(!showItem);
+  };
   return (
     <div className="w-1/2 bg-gray-50 shadow-xl p-4 my-4 mx-auto rounded-2xl">
       {/* HEader */}
-      <div className=" flex justify-between mx-auto">
+      <div
+        className=" flex justify-between mx-auto cursor-pointer"
+        onClick={handleClick}
+      >
         <span className="font-bold text-lg">
           {data.title} ({data.itemCards.length})
         </span>
         <span>🔽</span>
       </div>
-      {/* Accordian Body */}
 
-      <ItemList items={data.itemCards} />
+      {/* Accordian Body */}
+      {showItem && <ItemList items={data.itemCards} />}
     </div>
   );
 };
